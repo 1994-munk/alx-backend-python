ffbef13917b11bade61d5ef43a043e03a25aa5cf
Shirleen Gatu <wambuimunk@gmail.com>
2025-09-21 17:28:47 +0300

    Setup models, serializers, views and ran initial server

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path,include
 from .views import ConversationViewSet, MessageViewSet  # ✅ correct import
 from rest_framework.routers import DefaultRouter
-
+from rest_framework import routers
 
 # Use DRF router to auto-generate routes
-router = "routers.DefaultRouter()"
+router = routers.DefaultRouter()
 router.register(r'conversations', ConversationViewSet, basename='conversation')
 router.register(r'messages', MessageViewSet, basename='message')
 
