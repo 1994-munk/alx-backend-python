405ba84fd046282af04aec3d7383961afb821b19
Shirleen Gatu <wambuimunk@gmail.com>
2025-09-21 17:36:36 +0300

    Setup models, serializers, views and ran initial server

--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -20,5 +20,7 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('chats.urls')),  # Include chats app routes
+    path('api-auth/', include('rest_framework.urls')),  # for login/logout in DRF
+
 
 ]
